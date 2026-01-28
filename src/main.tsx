f26bd5d204706e0dea3ff75c7dd1217f369137d6
Erik Benton <erik.benton@hotmail.com>
2026-01-28 10:33:01 -0800

    cleaning up Vites temp files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import CssBaseline from "@mui/material/CssBaseline";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <CssBaseline />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>,
-)
+);
