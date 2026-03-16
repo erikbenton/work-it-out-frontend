b2af41c2427774bc9f0f513f70aa1cacbe47a6d8
Erik Benton <erik.benton@hotmail.com>
2026-03-16 13:07:30 -0700

    cleaning up appbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,14 +2,9 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import { useState, type ReactElement } from "react";
 import { Link } from "react-router-dom";
 import Drawer from "@mui/material/Drawer";
@@ -19,6 +14,7 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import NoteAltIcon from '@mui/icons-material/NoteAlt';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import { Typography } from "@mui/material";
 
 export type PageLink = {
   text: string,
@@ -33,30 +29,20 @@ const pages: PageLink[] = [
   { text: "History", url: "/completedWorkouts", icon: <AssignmentTurnedInIcon color="primary" /> },
   { text: "Training", url: "/activeWorkout", icon: <DirectionsRunIcon color="primary" /> },
 ];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 export default function Navbar() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Container maxWidth="xl">
         <Toolbar
           disableGutters
@@ -94,38 +80,12 @@ export default function Navbar() {
             ))}
           </Box>
 
-          {/* User Profile menu */}
-          <Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>U</Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+          <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+            <Typography variant="h5" alignContent='center'>
+              Work-It-Out
+            </Typography>
           </Box>
+
         </Toolbar>
       </Container>
     </AppBar>
