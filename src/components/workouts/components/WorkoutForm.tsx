23b96f50754b0470607364bd3f934c03a14dc9aa
Erik Benton <erik.benton@hotmail.com>
2026-04-26 11:54:50 -0700

    adding margin to workout form to match other layout on other pages

--- src/components/workouts/components/WorkoutForm.tsx
+++ src/components/workouts/components/WorkoutForm.tsx
@@ -7,7 +7,7 @@ export default function WorkoutForm() {
   const { workout, editing } = useWorkoutForm();
 
   return (
-    <Box className="w-full md:w-2/3" role={editing ? 'form' : 'div'}>
+    <Box className="w-full md:w-2/3" sx={{ mt: 1 }} role={editing ? 'form' : 'div'}>
       <WorkoutFormTitle />
       <Stack spacing={1} sx={{ pb: 3, px: 1 }} >
         {workout.exerciseGroups.map(group => (
