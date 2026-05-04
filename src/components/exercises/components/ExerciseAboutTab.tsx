42c9cc31a50cf83aca268995cf59ddcbca3eee64
Erik Benton <erik.benton@hotmail.com>
2026-05-04 08:25:37 -0700

    removing muscle text from Exercise details since the pie chart shows them

--- src/components/exercises/components/ExerciseAboutTab.tsx
+++ src/components/exercises/components/ExerciseAboutTab.tsx
@@ -16,7 +16,6 @@ export default function ExerciseAboutTab({ exercise }: Props) {
 
   return (
     <Box className="pt-2 px-3">
-      <Typography variant="body1" className='capitalize mb-2'>Muscles: {exercise?.muscles?.map(m => m.name).join(', ')}</Typography>
       <Typography variant="body1" className='capitalize mb-2'>Equipment: {exercise?.equipment}</Typography>
       <Typography variant="body1">Instructions: {exercise?.instructions ?? "No instructions"}</Typography>
       <PieChart sx={{ mt: 3 }} series={[{ data: muscleData }]}>
