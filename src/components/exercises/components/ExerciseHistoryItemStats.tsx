3722b80187028b88aecdc63660ac00cda6907a26
Erik Benton <erik.benton@hotmail.com>
2026-05-13 08:45:13 -0700

    finishing init implementation of updated ExerciseHistoryStats chips for completed exercise groups

--- src/components/exercises/components/ExerciseHistoryItemStats.tsx
+++ src/components/exercises/components/ExerciseHistoryItemStats.tsx
@@ -1,49 +1,26 @@
-import { Chip, ListItemText, Stack, Typography } from "@mui/material";
-import type { CompletedExerciseSet } from "../../../types/completedExerciseSet";
+import { Chip, ListItemText, Stack, Tooltip, Typography } from "@mui/material";
 import type { CompletedExerciseGroup } from "../../../types/completedExerciseGroup";
-import { calculateEstimatedOneRepMax } from "../../../utils/charts";
-import { red } from "@mui/material/colors";
-import { durationToHhMmSs, durationToSeconds, secondsToDuration } from "../../../utils/formatters";
+import { teal, green, red, cyan } from "@mui/material/colors";
+import { durationToHhMmSs, secondsToDuration } from "../../../utils/formatters";
 import { useExercises } from "../../../hooks/useExercises";
 import TimerIcon from '@mui/icons-material/Timer';
 import WatchLaterRoundedIcon from '@mui/icons-material/WatchLaterRounded';
 import RoomSharpIcon from '@mui/icons-material/RoomSharp';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import EmojiEventsSharpIcon from '@mui/icons-material/EmojiEventsSharp';
+import { calculateLiftStats, calculateStretchStats, calculateTimedStats } from "../../../utils/exerciseStats";
 
 type Props = {
   group: CompletedExerciseGroup
 }
 
-const totalReps = (sets: CompletedExerciseSet[]) => {
-  return sets.reduce((acc, curr) => acc + (curr.reps ?? 0), 0)
-}
-const totalVolume = (sets: CompletedExerciseSet[]) => {
-  return sets.reduce((acc, curr) => acc + (curr.weight ?? 0) * (curr.reps ?? 0), 0)
+const statsChipStyle = {
+  color: '#fff', py: 1.75, pr: 0.75, cursor: 'pointer'
 };
-const totalDistance = (sets: CompletedExerciseSet[]) => {
-  return sets.reduce((acc, curr) => acc + (curr.distance ?? 0), 0);
-}
-const totalTime = (sets: CompletedExerciseSet[]) => {
-  const totalSeconds = sets.reduce((acc, curr) => acc + durationToSeconds(curr.duration), 0);
-  const duration = secondsToDuration(totalSeconds, { format: 'include-hours' });
-  return durationToHhMmSs(duration, 'short');
-}
-const bestPace = (sets: CompletedExerciseSet[]): string | undefined => {
-  const pace = sets.reduce((acc, curr) => {
-    if (!curr.duration || !curr.distance) return acc;
-    const seconds = durationToSeconds(curr.duration);
-    const pace = seconds / curr.distance;
-    return !acc || pace < acc ? pace : acc;
-  }, undefined as (undefined | number));
-  if (!pace) return undefined;
-  const duration = secondsToDuration(pace);
-  return durationToHhMmSs(duration);
-}
 
 export default function ExerciseHistoryItemStats({ group }: Props) {
   const { services: exerciseServices } = useExercises();
-  const reps = totalReps(group.completedExerciseSets);
-  const weight = totalVolume(group.completedExerciseSets);
-  const oneRepMax = calculateEstimatedOneRepMax(group);
   const exercise = exerciseServices.getExerciseById(group.exerciseId);
 
   switch (exercise.categoryId) {
@@ -62,104 +39,111 @@ export default function ExerciseHistoryItemStats({ group }: Props) {
     default:
       return (<>No Stats available for exercise category: {exercise.categoryId}</>);
   }
-
-  return (
-    <ListItemText id={`list-label-${group.id}`} className='px-3' primary={
-      <Stack direction='row' spacing={1}>
-        <Chip
-          size="small"
-          label={`Reps: ${reps}`}
-          variant='filled'
-          color='success'
-        />
-        <Chip
-          size="small"
-          label={`Volume: ${weight} lbs`}
-          variant='filled'
-          color='primary'
-        />
-        <Chip
-          size="small"
-          label={`Est. 1RM: ${oneRepMax} lbs`}
-          variant='filled' sx={{ backgroundColor: red[600], color: '#fff' }}
-        />
-      </Stack>
-    } />
-  );
 }
 
 export function ExerciseHistoryLiftGroupStats({ group }: Props) {
-  const reps = totalReps(group.completedExerciseSets);
-  const volume = totalVolume(group.completedExerciseSets);
-  const oneRepMax = calculateEstimatedOneRepMax(group);
+  const { totalReps, totalVolume, oneRepMax } = calculateLiftStats(group);
 
   return (
     <ListItemText id={`list-label-${group.id}`} className='px-3' primary={
       <Stack direction='row' spacing={1}>
-        <Chip
-          size="small"
-          label={`Reps: ${reps}`}
-          variant='filled'
-          color='success'
-        />
-        <Chip
-          size="small"
-          label={`Volume: ${volume} lbs`}
-          variant='filled'
-          color='primary'
-        />
-        <Chip
-          size="small"
-          label={`Est. 1RM: ${oneRepMax} lbs`}
-          variant='filled'
-          sx={{ backgroundColor: red[600], color: '#fff' }}
-        />
+        {totalReps &&
+          <Tooltip title="Total Reps">
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <DoneAllIcon fontSize="small" sx={{ mr: 0.75 }} />{totalReps}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: green[700] }}
+            />
+          </Tooltip>
+        }
+        {totalVolume &&
+          <Tooltip title="Total Volume">
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <FitnessCenterIcon fontSize="small" sx={{ mr: 0.75 }} />{`${Math.round(totalVolume)} lbs`}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: cyan[700] }}
+            />
+          </Tooltip>
+        }
+        {oneRepMax !== undefined && oneRepMax > 0 &&
+          <Tooltip title='Estimated 1 Rep Max'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <EmojiEventsSharpIcon fontSize="small" sx={{ mr: 0.5 }} />{`${Math.round(oneRepMax)} lbs`}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: red[600] }}
+            />
+          </Tooltip>
+        }
       </Stack>
     } />
   );
 }
 
 export function ExerciseHistoryTimedGroupStats({ group }: Props) {
-  const distance = totalDistance(group.completedExerciseSets);
-  const time = totalTime(group.completedExerciseSets);
-  const pace = bestPace(group.completedExerciseSets);
+  const { totalDistance, totalSeconds, bestPaceSeconds} = calculateTimedStats(group);
+  const totalTime = totalSeconds ? secondsToDuration(totalSeconds) : undefined;
+  const bestPace = bestPaceSeconds ? secondsToDuration(bestPaceSeconds) : undefined;
 
   return (
     <ListItemText id={`list-label-${group.id}`} className='px-3' primary={
       <Stack direction='row' spacing={1}>
-        <Chip
-          size="small"
-          label={
-            <Typography variant="body2">
-              <RoomSharpIcon fontSize="small" />{`${distance.toFixed(2)} mi`}
-            </Typography>
-          }
-          variant='filled'
-          color='success'
-          sx={{ py: 1.75, pr: 0.75 }}
-        />
-        <Chip
-          size="small"
-          label={
-            <Typography variant="body2">
-              <TimerIcon fontSize="small" />{`${time}`}
-            </Typography>
-          }
-          variant='filled'
-          color="primary"
-          sx={{ py: 1.75, pr: 0.75 }}
-        />
-        {pace &&
-          <Chip
-            size="small"
-            label={
-              <Typography variant="body2">
-                <WatchLaterRoundedIcon fontSize="small" />{`${pace} / mi`}
-              </Typography>
-            }
-            variant='filled'
-            sx={{ backgroundColor: red[600], color: '#fff', py: 1.75, pr: 0.75 }}
-          />
+        {totalDistance &&
+          <Tooltip title='Total Distance'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <RoomSharpIcon fontSize="small" sx={{ mr: 0.5 }} />{`${totalDistance.toFixed(2)} mi`}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: teal[600] }}
+            />
+          </Tooltip>
+        }
+        {totalTime &&
+          <Tooltip title='Total Duration'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <WatchLaterRoundedIcon fontSize="small" sx={{ mr: 0.33 }} />{durationToHhMmSs(totalTime, 'short')}
+                </Typography>
+              }
+              variant='filled'
+              color="primary"
+              sx={{ ...statsChipStyle }}
+            />
+          </Tooltip>
+        }
+        {bestPace &&
+          <Tooltip title='Fastest Pace'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <TimerIcon fontSize="small" sx={{ mr: 0.25 }} />{`${durationToHhMmSs(bestPace)} / mi`}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: red[600] }}
+            />
+          </Tooltip>
         }
       </Stack>
     } />
@@ -167,24 +151,41 @@ export function ExerciseHistoryTimedGroupStats({ group }: Props) {
 }
 
 export function ExerciseHistoryStretchGroupStats({ group }: Props) {
-  const reps = totalReps(group.completedExerciseSets);
-  const weight = totalVolume(group.completedExerciseSets);
+  const { totalReps, totalSeconds } = calculateStretchStats(group);
+  const totalTime = totalSeconds ? secondsToDuration(totalSeconds) : undefined;
 
   return (
     <ListItemText id={`list-label-${group.id}`} className='px-3' primary={
       <Stack direction='row' spacing={1}>
-        <Chip
-          size="small"
-          label={`Reps: ${reps}`}
-          variant='filled'
-          color='success'
-        />
-        <Chip
-          size="small"
-          label={`Volume: ${weight} lbs`}
-          variant='filled'
-          color="primary"
-        />
+        {totalReps &&
+          <Tooltip title='Total Reps'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <DoneAllIcon fontSize="small" sx={{ mr: 0.75 }} />{totalReps}
+                </Typography>
+              }
+              variant='filled'
+              sx={{ ...statsChipStyle, backgroundColor: green[700] }}
+            />
+          </Tooltip>
+        }
+        {totalTime &&
+          <Tooltip title='Total Duration'>
+            <Chip
+              size="small"
+              label={
+                <Typography variant="body2">
+                  <WatchLaterRoundedIcon fontSize="small" sx={{ mr: 0.33 }} />{durationToHhMmSs(totalTime, 'short')}
+                </Typography>
+              }
+              variant='filled'
+              color='primary'
+              sx={{ py: 1.75, pr: 0.75 }}
+            />
+          </Tooltip>
+        }
       </Stack>
     } />
   );
---
+++ src/utils/exerciseStats.ts
@@ -0,0 +1,97 @@
+import type { CompletedExerciseGroup } from "../types/completedExerciseGroup";
+import { calculateEstimatedOneRepMax } from "./charts";
+import { durationToSeconds } from "./formatters";
+
+type LiftStats = {
+  totalReps?: number,
+  totalVolume?: number,
+  oneRepMax?: number
+}
+
+type TimedStats = {
+  totalDistance?: number,
+  totalSeconds?: number,
+  bestPaceSeconds?: number
+}
+
+type StretchStats = {
+  totalReps?: number,
+  totalSeconds?: number
+}
+
+const initLiftStats: LiftStats = {
+  totalReps: undefined,
+  totalVolume: undefined,
+  oneRepMax: undefined
+};
+
+const initTimedStats: TimedStats = {
+  totalDistance: undefined,
+  totalSeconds: undefined,
+  bestPaceSeconds: undefined
+};
+
+const initStretchStats: StretchStats = {
+  totalReps: undefined,
+  totalSeconds: undefined
+};
+
+export const calculateLiftStats = (group: CompletedExerciseGroup) => {
+  const stats = group.completedExerciseSets.reduce((acc, curr) => {
+    const newStats = { ...initLiftStats };
+    newStats.totalReps = curr.reps === undefined
+      ? acc.totalReps
+      : (acc.totalReps ?? 0) + curr.reps;
+
+    newStats.totalVolume = (curr.weight === undefined || curr.reps === undefined)
+      ? acc.totalVolume
+      : (acc.totalVolume ?? 0) + (curr.reps * curr.weight);
+
+    return newStats;
+  }, initLiftStats);
+  stats.oneRepMax = calculateEstimatedOneRepMax(group);
+  return stats;
+}
+
+export const calculateTimedStats = (group: CompletedExerciseGroup) => {
+  const stats = group.completedExerciseSets.reduce((acc, curr) => {
+    const newStats = { ...initTimedStats };
+    newStats.totalDistance = curr.distance === undefined
+      ? acc.totalDistance
+      : (acc.totalDistance ?? 0) + curr.distance;
+
+    newStats.totalSeconds = !curr.duration
+      ? acc.totalSeconds
+      : (acc.totalSeconds ?? 0) + durationToSeconds(curr.duration);
+
+    if (curr.distance && curr.duration !== undefined) {
+      const pace = durationToSeconds(curr.duration) / curr.distance;
+      newStats.bestPaceSeconds = !acc.bestPaceSeconds || pace < acc.bestPaceSeconds
+        ? pace
+        : acc.bestPaceSeconds;
+    } else {
+      newStats.bestPaceSeconds = acc.bestPaceSeconds;
+    }
+
+    return newStats;
+  }, initTimedStats);
+
+  return stats;
+}
+
+export const calculateStretchStats = (group: CompletedExerciseGroup) => {
+  const stats = group.completedExerciseSets.reduce((acc, curr) => {
+    const newStats = { ...initStretchStats };
+    newStats.totalReps = !curr.reps
+      ? acc.totalReps
+      : (acc.totalReps ?? 0) + curr.reps;
+
+    newStats.totalSeconds = !curr.duration
+      ? acc.totalSeconds
+      : (acc.totalSeconds ?? 0) + durationToSeconds(curr.duration);
+
+    return newStats;
+  }, initStretchStats);
+
+  return stats;
+}
