c95e16623bf9f1d26de0ab77ba63585540fc6b71
Erik Benton <erik.benton@hotmail.com>
2026-05-15 13:16:58 -0700

    updating Exercise Stats icon for new exercise categories

--- src/components/exercises/components/ExerciseStatsBar.tsx
+++ src/components/exercises/components/ExerciseStatsBar.tsx
@@ -6,28 +6,79 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DownloadDoneIcon from '@mui/icons-material/DownloadDone';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import TimelineIcon from '@mui/icons-material/Timeline';
-import { blue, cyan, green, grey, pink } from "@mui/material/colors";
-import { calculateEstimatedOneRepMax, calculateVolume } from "../../../utils/charts";
+import TimerIcon from '@mui/icons-material/Timer';
+import { blue, cyan, green, grey, pink, teal } from "@mui/material/colors";
+import WatchLaterRoundedIcon from '@mui/icons-material/WatchLaterRounded';
+import RoomSharpIcon from '@mui/icons-material/RoomSharp';
 import type { CompletedExerciseGroup } from "../../../types/completedExerciseGroup";
-import { formatLargeNumber } from "../../../utils/formatters";
+import { durationToHhMmSs, formatLargeNumber, secondsToDuration } from "../../../utils/formatters";
+import { useExercises } from "../../../hooks/useExercises";
+import { calculateLiftStats, calculateStretchStats, calculateTimedStats } from "../../../utils/exerciseStats";
 
 type Props = {
   history: CompletedExerciseGroup[]
 }
 
+type ExerciseStats = {
+  sets: number,
+  reps?: number,
+  oneRepMax?: number,
+  volume?: number,
+  distance?: number,
+  seconds?: number,
+  bestPaceSeconds?: number,
+  time?: string,
+  bestPace?: string
+}
+
+const initializeExerciseStats = (): ExerciseStats => {
+  return {
+    sets: 0
+  }
+}
+
 export default function ExerciseStatsBar({ history }: Props) {
-  const totalSets = history.reduce((acc, group) => acc + group.completedExerciseSets.length, 0);
-  const totalReps = history.reduce((acc, group) => (
-    acc + group.completedExerciseSets.reduce((acc, set) => acc + (set.reps ?? 0), 0)
-  ), 0);
-  const totalVolume = history.reduce((acc, group) => acc + calculateVolume(group), 0);
-  const oneRepMax = history.reduce((acc, group) => {
-    const oneRep = calculateEstimatedOneRepMax(group);
-    return oneRep > acc ? oneRep : acc;
-  }, 0);
+  const { services: exerciseServices } = useExercises();
+
+  const stats = initializeExerciseStats();
+  history.forEach(group => {
+    stats.sets += group.completedExerciseSets.length;
+    const exercise = exerciseServices.getExerciseById(group.exerciseId);
+    if (exercise.category === 'lift') {
+      const { totalReps, totalVolume, oneRepMax } = calculateLiftStats(group);
+      stats.reps = totalReps
+        ? (stats.reps ?? 0) + totalReps
+        : stats.reps;
+      stats.oneRepMax = oneRepMax
+        ? oneRepMax > (stats.oneRepMax ?? 0) ? oneRepMax : stats.oneRepMax
+        : stats.oneRepMax;
+      stats.volume = totalVolume
+        ? (stats.volume ?? 0) + totalVolume
+        : stats.volume;
+    } else if (exercise.category === 'timed') {
+      const { totalDistance, totalSeconds, bestPaceSeconds } = calculateTimedStats(group);
+      stats.distance = totalDistance
+        ? (stats.distance ?? 0) + totalDistance
+        : stats.distance;
+      stats.seconds = totalSeconds
+        ? (stats.seconds ?? 0) + totalSeconds
+        : stats.seconds;
+      stats.bestPaceSeconds = bestPaceSeconds
+        ? bestPaceSeconds <= (stats.bestPaceSeconds ?? bestPaceSeconds) ? bestPaceSeconds : stats.bestPaceSeconds
+        : stats.bestPaceSeconds;
+    } else if (exercise.category === 'stretch' || exercise.category === 'conditioning') {
+      const { totalReps } = calculateStretchStats(group);
+      stats.reps = totalReps
+        ? (stats.reps ?? 0) + totalReps
+        : stats.reps;
+    }
+  });
+
+  stats.time = stats.seconds && stats.seconds > 0 ? secondsToDuration(stats.seconds) : undefined;
+  stats.bestPace = stats.bestPaceSeconds && stats.bestPaceSeconds > 0 ? secondsToDuration(stats.bestPaceSeconds) : undefined;
 
   return (
-    <Stack direction='row' spacing={1} sx={{ overflowX: 'auto', justifyContent: 'space-around' }}>
+    <Stack direction='row' sx={{ overflowX: 'auto', justifyContent: 'space-around' }}>
       <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
         <Avatar sx={{ bgcolor: blue[700] }}>
           <TimelineIcon />
@@ -42,37 +93,70 @@ export default function ExerciseStatsBar({ history }: Props) {
           <FormatListBulletedIcon />
         </Avatar>
         <Typography variant="body1">
-          {formatLargeNumber(totalSets, totalSets >= 10_000)}
+          {formatLargeNumber(stats.sets, stats.sets >= 10_000)}
         </Typography>
         <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Sets</Typography>
       </Stack>
-      <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
-        <Avatar sx={{ bgcolor: green[700] }}>
-          <DoneAllIcon />
-        </Avatar>
-        <Typography variant="body1">
-          {formatLargeNumber(totalReps, totalReps >= 10_000)}
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Reps</Typography>
-      </Stack>
-      <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
-        <Avatar sx={{ bgcolor: pink[700] }}>
-          <DownloadDoneIcon />
-        </Avatar>
-        <Typography variant="body1">
-          {Math.round(oneRepMax)} lbs
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Est. 1RM</Typography>
-      </Stack>
-      <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
-        <Avatar sx={{ bgcolor: cyan[700] }}>
-          <FitnessCenterIcon />
-        </Avatar>
-        <Typography variant="body1">
-          {formatLargeNumber(totalVolume, totalVolume >= 10_000)} lbs
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Volume</Typography>
-      </Stack>
+      {stats.reps && stats.reps > 0 &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: green[700] }}>
+            <DoneAllIcon />
+          </Avatar>
+          <Typography variant="body1">
+            {formatLargeNumber(stats.reps, stats.reps >= 10_000)}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Reps</Typography>
+        </Stack>
+      }
+      {stats.oneRepMax && stats.oneRepMax > 0 &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: pink[700] }}>
+            <DownloadDoneIcon />
+          </Avatar>
+          <Typography variant="body1">
+            {Math.round(stats.oneRepMax)} lbs
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Est. 1RM</Typography>
+        </Stack>
+      }
+      {stats.volume && stats.volume > 0 &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: cyan[700] }}>
+            <FitnessCenterIcon />
+          </Avatar>
+          <Typography variant="body1">
+            {formatLargeNumber(stats.volume, stats.volume >= 10_000)} lbs
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }} textAlign='center'>Volume</Typography>
+        </Stack>
+      }
+      {stats.distance !== undefined && stats.distance > 0 &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: teal[700] }}>
+            <RoomSharpIcon />
+          </Avatar>
+          <Typography variant="body1" noWrap>{stats.distance} mi</Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }}>Distance</Typography>
+        </Stack>
+      }
+      {stats.time !== undefined &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: blue[700] }}>
+            <TimerIcon />
+          </Avatar>
+          <Typography variant="body1" noWrap>{durationToHhMmSs(stats.time, 'short')}</Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }}>Time</Typography>
+        </Stack>
+      }
+      {stats.bestPace !== undefined &&
+        <Stack alignItems='center' sx={{ p: 1, pb: 0 }}>
+          <Avatar sx={{ bgcolor: pink[700] }}>
+            <WatchLaterRoundedIcon />
+          </Avatar>
+          <Typography variant="body1" noWrap>{durationToHhMmSs(stats.bestPace)}</Typography>
+          <Typography variant="body2" sx={{ color: 'gray' }}>Best Pace</Typography>
+        </Stack>
+      }
     </Stack>
   );
 }
